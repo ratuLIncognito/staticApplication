c8d33a1207c862fefdaac2eae0be8a36434085ca
Ratul Antik Das <ratulcse09buet@gmail.com>
2015-09-07 22:27:20 +0600

    initial project version

---
+++ StaticApplication.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package staticapplication;
+
+/**
+ *
+ * @author Antu
+ */
+
+public class StaticApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
