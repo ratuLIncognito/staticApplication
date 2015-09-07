c8d73ab41531be9b3b8ce304c63526490cb2ec4e
ratuLIncognito <ratulcse09buet@gmail.com>
2015-09-07 22:58:35 +0600

    Version 2

--- StaticApplication.java
+++ StaticApplication.java
@@ -18,5 +18,6 @@ public class StaticApplication {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("My application");
     }
 }
